6cf01ed93047fc8a081328e37bd5115d453c78cb
Natalia Bento <nataliabento97@gmail.com>
2025-07-02 15:28:41 -0300

    feat: are admin do django

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Produto, Cliente, Categoria
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'preco', 'estoque')
+    
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'sobrenome', 'email')
+    
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'descricao')
+
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Cliente, ClienteAdmin)
+admin.site.register(Categoria, CategoriaAdmin)
--- django1/urls.py
+++ django1/urls.py
@@ -21,6 +21,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('painel/', admin.site.urls),
     path('', include('core.urls')),
 ]
